d052d22ce5f92a5944349a06dfc60efdfb71c0a8
Anna Romashenko <aa.romashchenko@gmail.com>
2020-05-05 22:37:37 +0300

    Updates

---
+++ precache-manifest.a1d8166865780a23d3b753d59258235e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c0b7a77aabaa5db105e73c97afd52c",
+    "url": "/react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "1725d392fa7baf5b2267",
+    "url": "/react-hw-03-phonebook/static/js/2.9d13a756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-03-phonebook/static/js/2.9d13a756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6f0b5bcb5a69c55c92",
+    "url": "/react-hw-03-phonebook/static/js/main.542c182a.chunk.js"
+  },
+  {
+    "revision": "a023d8580ed29e4b42ad",
+    "url": "/react-hw-03-phonebook/static/js/runtime-main.3f23fc17.js"
+  }
+]);
